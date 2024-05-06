62991d7e0f0c48da2ee67fa95eacc03dfc42b0cf
Paul Schüller <136228264+pschyl@users.noreply.github.com>
2024-05-06 15:15:43 +0200

    add petcardcss

--- frontend/src/components/PetCard.tsx
+++ frontend/src/components/PetCard.tsx
@@ -1,7 +1,8 @@
 import {Pet} from "../model/Pet.ts";
+import './PetCardCSS.css'
 
 export function PetCard(props:Readonly<Pet>) {
-    return <div>
+    return <div className={"petCard"}>
         <img src={props.images[0]}/>
         <p>{props.name}</p>
     </div>
