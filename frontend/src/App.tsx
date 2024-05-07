cb8196e9ab434be54d608b7175732f066cc551fb
Paul Schüller <136228264+pschyl@users.noreply.github.com>
2024-05-07 11:18:26 +0200

    basic design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,9 +25,37 @@ function App() {
 
   return (
     <>
-        {petList.map((pet: Pet) => (
-        <PetCard id={pet.id} name={pet.name} species={pet.species} images={pet.images} />
-        ))}
+        <header>
+            <div className={"logo_login_container"}>
+                <div>Logo</div>
+                <div>Login</div>
+            </div>
+            <div>
+                Navbar
+            </div>
+        </header>
+        <main>
+            <div className={"inputBar_container"}>
+                <form className={"inputBar"}>
+                    <input className={"inputBar_element"} type={"text"}/>
+                    <select>
+                        <option>5km</option>
+                        <option>10km</option>
+                        <option>20km</option>
+                        <option>50km</option>
+                    </select>
+                </form>
+            </div>
+            <div className={"petCard_container"}>
+                {petList.map((pet: Pet) => (
+                    <PetCard id={pet.id} name={pet.name} species={pet.species} images={pet.images} />
+                ))}
+            </div>
+        </main>
+        <footer>
+            Footer
+        </footer>
+
     </>
   )
 }
--- frontend/src/components/PetCard.tsx
+++ frontend/src/components/PetCard.tsx
@@ -3,7 +3,7 @@ import './PetCardCSS.css'
 
 export function PetCard(props:Readonly<Pet>) {
     return <div className={"petCard"}>
-        <img src={props.images[0]}/>
+        <div className={"image_container"}><img src={props.images[0]}/></div>
         <p>{props.name}</p>
     </div>
 }
