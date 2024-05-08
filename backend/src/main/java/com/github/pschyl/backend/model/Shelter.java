9fea3fc08d4de1e0615b5b8792065e80be38bcd3
Paul Schüller <136228264+pschyl@users.noreply.github.com>
2024-05-08 13:57:34 +0200

    add Shelter Class

---
+++ backend/src/main/java/com/github/pschyl/backend/model/Shelter.java
@@ -0,0 +1,14 @@
+package com.github.pschyl.backend.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Shelter {
+
+    private final String id;
+    private final String name;
+    private final String postalCode;
+
+}
