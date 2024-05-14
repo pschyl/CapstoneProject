38a45de4e62d56c49ac7f7f8d3b9e1c60785ad92
Paul Schüller <136228264+pschyl@users.noreply.github.com>
2024-05-14 10:52:19 +0200

    add to details with click on image

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import {LogoLogin} from "./components/LogoLogin.tsx";
 import {NavBar} from "./components/NavBar.tsx";
 import {Route, Routes} from "react-router-dom";
 import FindPetPage from "./pages/findPetPage.tsx";
+import FindPetDetailPage from "./pages/findPetDetailPage.tsx";
 
 export default App
 
@@ -18,6 +19,7 @@ function App() {
         <main>
             <Routes>
                 <Route path={"/"} element={<FindPetPage />} />
+                <Route path={"/find/:id"} element={<FindPetDetailPage />} />
             </Routes>
         </main>
         <footer>
--- frontend/src/components/PetCard.tsx
+++ frontend/src/components/PetCard.tsx
@@ -1,9 +1,16 @@
 import {Pet} from "../model/Pet.ts";
 import './PetCard.css'
+import {useNavigate} from "react-router-dom";
 
 export function PetCard(props:Readonly<Pet>) {
+
+    const navigate = useNavigate()
+    function showDetails() {
+        navigate("/find/" + props.id)
+    }
+
     return <div className={"petCard"}>
-        <div className={"image_container"}><img src={props.images[0]}/></div>
+        <div className={"image_container"}><img onClick={showDetails} src={props.images[0]}/></div>
         <div className={"cardInformation"}>
             <div id={"name"}>{props.name}</div>
             <div id={"shelter_name"}>{props.shelter.name}</div>
