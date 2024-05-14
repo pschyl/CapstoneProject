3bedc6c31abd85684c9b434b051d5577033dce60
Paul Schüller <136228264+pschyl@users.noreply.github.com>
2024-05-14 10:19:21 +0200

    reorganize in route system

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 export function NavBar() {
     return <nav>
-        <Link className={"navBar_element"} to={"/"}>Finden</Link>
+        <Link className={"navBar_element"} to={"/find"}>Finden</Link>
         <Link className={"navBar_element"} to={"/new"}>Vermitteln</Link>
         <Link className={"navBar_element"} to={"/support"}>Support</Link>
     </nav>
