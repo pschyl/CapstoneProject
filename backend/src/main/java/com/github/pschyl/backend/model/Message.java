6a486af4a529a859537d3543c520af29be8a0b39
Paul Schüller <136228264+pschyl@users.noreply.github.com>
2024-05-24 11:36:58 +0200

    init Message model

---
+++ backend/src/main/java/com/github/pschyl/backend/model/Message.java
@@ -0,0 +1,18 @@
+package com.github.pschyl.backend.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@RequiredArgsConstructor
+public class Message {
+
+    private final String id;
+    private final String addressee;
+    private final String recipient;
+    private final String message;
+    private final LocalDateTime timestamp;
+
+}
