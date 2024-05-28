cba1a248e58527005a8fdad654154ec1e3788905
Paul Schüller <136228264+pschyl@users.noreply.github.com>
2024-05-28 12:55:11 +0200

    post message works

--- frontend/src/components/NewMessage.tsx
+++ frontend/src/components/NewMessage.tsx
@@ -1,16 +1,56 @@
 import './NewMessage.css'
+import {Message} from "../model/Message.ts";
+import {ChangeEvent, FormEvent, useState} from "react";
+import axios from "axios";
+
 
 type NewMessageProps = {
     setPopup: (Popup: boolean) => void
+    username: string
 }
 
 export function NewMessage(props: NewMessageProps) {
+
+    const [newMessage, setNewMessage] = useState<Message>({
+        id: "",
+        addressee: props.username,
+        recipient: "",
+        header: "",
+        message: "",
+        timestamp: "",
+    })
+
+    function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        const key = event.target.name
+        setNewMessage({...newMessage, [key]: event.target.value})
+    }
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        console.log("Hallo")
+        console.log(newMessage)
+        event.preventDefault()
+        axios.post("/api/messages", newMessage)
+            .then(() => {setNewMessage({
+                id: "",
+                addressee: props.username,
+                recipient: "",
+                header: "",
+                message: "",
+                timestamp: "",
+            }
+            )})
+            .then(() => props.setPopup(false))
+            .catch(e => {console.log(e.message)})
+    }
+
+
+
     return <div className={"new_message_card"}>
-        <form className={"new_message_form"}>
-            <input placeholder={"Empfänger"}/>
-            <input placeholder={"Betreff"}/>
-            <textarea placeholder={"Max. 440 Zeichen"} rows={7} cols={55} maxLength={440}></textarea>
-            <button>Abschicken</button>
+        <form onSubmit={handleSubmit} className={"new_message_form"}>
+            <input onChange={handleInputChange} name={"recipient"} placeholder={"Empfänger"} value={newMessage.recipient}/>
+            <input onChange={handleInputChange} placeholder={"Betreff"} name={"header"} value={newMessage.header}/>
+            <textarea onChange={handleInputChange} placeholder={"Max. 440 Zeichen"} name={"message"} rows={7} cols={55} maxLength={440} value={newMessage.message}></textarea>
+            <button type={"submit"}>Abschicken</button>
         </form>
         <button className={"close_popup"} onClick={() => props.setPopup(false)}>❌</button>
     </div>
--- frontend/src/pages/yourMessagesPage.tsx
+++ frontend/src/pages/yourMessagesPage.tsx
@@ -7,6 +7,8 @@ import MessageCard from "../components/MessageCard.tsx";
 import './yourMessagesPage.css'
 import refresh from '../assets/refresh.png'
 import {NewMessage} from "../components/NewMessage.tsx";
+import add from '../assets/add2.png'
+import searchLogo from "../assets/search-icon.webp";
 
 type MessagesProps = {
     shelter:Shelter
@@ -35,9 +37,6 @@ export default function YourMessagesPage(props: MessagesProps) {
         }
     }
 
-    function handlePopup() {
-        setPopupIsOpen(true)
-    }
 
     function refreshPage() {
         if (refreshed) {
@@ -55,33 +54,38 @@ export default function YourMessagesPage(props: MessagesProps) {
 
     return <>
         <div className={"message_option_container"}>
-            <div>
-                <input
-                    type={"checkbox"}
-                    id={"received"}
-                    checked={isChecked[0]}
-                    onChange={() => switchDisplayedMessages(0)}
-                />
-                <label htmlFor={"received"}>Empfangen</label>
+            <button id={"add"} onClick={() => setPopupIsOpen(true)}><img src={add}/></button>
+            <div className={"messages_filter"}>
+                <div>
+                    <input
+                        type={"checkbox"}
+                        id={"received"}
+                        checked={isChecked[0]}
+                        onChange={() => switchDisplayedMessages(0)}
+                    />
+                    <label htmlFor={"received"}>Empfangen</label>
 
-                <input
-                    type={"checkbox"}
-                    id={"sent"}
-                    checked={isChecked[1]}
-                    onChange={() => switchDisplayedMessages(1)}
-                />
-                <label htmlFor={"sent"}>Gesendet</label>
+                    <input
+                        type={"checkbox"}
+                        id={"sent"}
+                        checked={isChecked[1]}
+                        onChange={() => switchDisplayedMessages(1)}
+                    />
+                    <label htmlFor={"sent"}>Gesendet</label>
+                </div>
+                <form>
+                    <input type={"text"} placeholder={"Suche"}/>
+                    <img id={"search_logo"} src={searchLogo}/>
+                </form>
             </div>
-            <div>
-                <input/>
-            </div>
-            <div>
-                <button onClick={handlePopup}>+</button>
+            <div className={"message_options"}>
                 <button id={"refresh"} onClick={refreshPage}><img src={refresh}/></button>
             </div>
         </div>
 
-        {popupIsOpen && <div className={"new_message_container"}><NewMessage setPopup={setPopupIsOpen}/></div>}
+        {popupIsOpen && <div className={"new_message_container"}><NewMessage setPopup={setPopupIsOpen}
+                                                                             username={props.shelter.userName + props.user.userName}/>
+        </div>}
 
         <div className={"message_card_container"}>
             {isChecked[0] ? messageList
